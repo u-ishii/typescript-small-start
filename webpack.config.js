3500cb522ca4efd2f463b7daa31a46f26d8febb2
u-ishii <yuki.ishii.business@gmail.com>
2020-05-27 22:04:22 +0900

    add comment

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,20 @@
-const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.ts',
+    entry: './src/index.ts', // 最初に読み込むスクリプト
     module: {
         rules: [
             {
                 test: /\.ts$/,
                 use: 'ts-loader',
                 exclude: /node_modules/,
-            },
+            }, // ts-loader で TS -> JS にする
         ],
     },
     resolve: {
-        extensions: ['.ts', '.js'],
+        extensions: ['.ts', '.js'], // import 時に省略できる拡張子
     },
     plugins: [
-        new HtmlWebpackPlugin(),
+        new HtmlWebpackPlugin(), // 空の HTML を生成するプラグイン
     ],
 }
