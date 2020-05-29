3b58fd4c9cc7b4d8b20d80e7146ef1ea80516c80
u-ishii <yuki.ishii.business@gmail.com>
2020-05-29 15:21:39 +0900

    split index.ts

---
+++ src/area-to-region.ts
@@ -0,0 +1,30 @@
+export { Area, Region, areaToRegion }
+
+/**
+ * 長方形の始点（左上）・終点（右下）の座標
+ */
+type Area = {
+    x1: number,
+    y1: number,
+    x2: number,
+    y2: number,
+}
+
+/**
+ * 長方形の始点（左上）と大きさ
+ */
+type Region = {
+    left: number,
+    top: number,
+    width: number,
+    height: number,
+}
+
+const areaToRegion = (area: Area): Region => {
+    return {
+        left: area.x1,
+        top: area.y1,
+        width: area.x2 - area.x1,
+        height: area.y2 - area.y1,
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,6 @@
-export { Area, Region, areaToRegion }
-
-/**
- * 長方形の始点（左上）・終点（右下）の座標
- */
-type Area = {
-    x1: number,
-    y1: number,
-    x2: number,
-    y2: number,
-}
-
-/**
- * 長方形の始点（左上）と大きさ
- */
-type Region = {
-    left: number,
-    top: number,
-    width: number,
-    height: number,
-}
-
-const areaToRegion = (area: Area): Region => {
-    return {
-        left: area.x1,
-        top: area.y1,
-        width: area.x2 - area.x1,
-        height: area.y2 - area.y1,
-    }
-}
+import { areaToRegion } from './area-to-region'
 
 console.log(areaToRegion({ x1: 1, y1: 1, x2: 2, y2: 2 }))
+
+// Property 'y2' is missing
+// console.log(areaToRegion({ x1: 1, y1: 1, x2: 2 }))
