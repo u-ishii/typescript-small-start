9d98b1275f263af9e3cddc4ce5087bb99abc5561
u-ishii <yuki.ishii.business@gmail.com>
2020-06-03 14:00:46 +0900

    rm index.spec.ts

--- src/area-to-region.spec.ts
+++ src/area-to-region.spec.ts
@@ -1,6 +1,6 @@
-import {areaToRegion} from './area-to-region'
+import { areaToRegion } from './area-to-region'
 
 test('OK', () => {
-    expect(areaToRegion({x1: 10, y1: 10, x2: 15, y2: 15}))
-        .toStrictEqual({left: 10, top: 10, width: 5, height: 5})
+    expect(areaToRegion({ x1: 10, y1: 10, x2: 15, y2: 15 }))
+        .toStrictEqual({ left: 10, top: 10, width: 5, height: 5 })
 })
