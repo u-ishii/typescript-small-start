4d641229ac3aeae8b00805668d9f7e98446a1918
u-ishii <yuki.ishii.business@gmail.com>
2020-06-03 15:50:51 +0900

    compress index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
 import { areaToRegion } from './area-to-region'
 
 console.log(areaToRegion({ x1: 1, y1: 1, x2: 2, y2: 2 }))
-
-// Property 'y2' is missing
-// console.log(areaToRegion({ x1: 1, y1: 1, x2: 2 }))
