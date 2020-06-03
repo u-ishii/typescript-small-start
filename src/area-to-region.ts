25d5e0e99d7a70742262e670078f06a3c58bda7b
u-ishii <yuki.ishii.business@gmail.com>
2020-06-03 13:49:25 +0900

    compress area-to-region

--- src/area-to-region.ts
+++ src/area-to-region.ts
@@ -1,22 +1,12 @@
 /**
  * 長方形の始点（左上）・終点（右下）の座標
  */
-type Area = {
-    x1: number,
-    y1: number,
-    x2: number,
-    y2: number,
-}
+type Area = { x1: number, y1: number, x2: number, y2: number, }
 
 /**
  * 長方形の始点（左上）と大きさ
  */
-type Region = {
-    left: number,
-    top: number,
-    width: number,
-    height: number,
-}
+type Region = { left: number, top: number, width: number, height: number, }
 
 const areaToRegion = (area: Area): Region => {
     return {
